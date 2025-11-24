20549afeb0e7f4d1143d39d12f19945e17d98afd
Hamza Anwer <hamza.anwer8@gmail.com>
2025-11-25 02:17:51 +0500

    added login api route

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,43 @@
+import User from "@/database/user.model";
+import connectDB from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
+
+interface SignupBody {
+    email: string;
+    password: string;
+    username: string;
+}
+
+export async function POST(req: NextRequest) {
+    try {
+        const userInfo: SignupBody = await req.json();
+        const { email, password } = userInfo;
+
+        await connectDB();
+
+        const user = await User.findOne({ email })
+
+        const comparedPass = await bcrypt.compare(password, user.password)
+
+        if (!user || !comparedPass) return NextResponse.json({ error: "Incorrect email or password" }, { status: 500 })
+
+        const tokenPayload = {
+            id: user._id,
+            email: user.email
+        }
+
+        const signedToken = jwt.sign(tokenPayload, process.env.AUTH_SECRET as string, { expiresIn: "1d" })
+
+
+        const response = NextResponse.json({ message: "Loggend In", success: true })
+
+        response.cookies.set("token", signedToken, { httpOnly: true, })
+
+        return response;
+    }
+    catch (error) {
+        return NextResponse.json({ error: error instanceof Error ? error.message : "" }, { status: 500 })
+    }
+}

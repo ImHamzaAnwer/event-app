6779b295b97d938c0d17d729efd65c9b582ae5a5
Hamza Anwer <hamza.anwer8@gmail.com>
2025-11-25 18:56:01 +0500

    add login and signup page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function SignupPage() {
+  const [user, setUser] = useState({ email: "", password: "" });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/login", user);
+      console.log(response);
+      router.push("/");
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-3">
+      <h3 className="text-center">Login</h3>
+
+      <input
+        className="p-2 border border-white"
+        placeholder="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <input
+        className="p-2 border border-white"
+        placeholder="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+
+      <button
+        type="button"
+        disabled={buttonDisabled}
+        onClick={onLogin}
+        className="p-2 rounded bg-white text-black"
+      >
+        {loading ? "loggin in..." : "Login"}
+      </button>
+
+      <Link className="text-center" href={"/signup"}>
+        Signup page
+      </Link>
+    </div>
+  );
+}

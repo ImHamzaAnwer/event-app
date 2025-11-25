564c69b71d1bfb1916cbc89579295de9fe83dd98
Hamza Anwar <hamza.anwer8@gmail.com>
2025-11-25 18:34:13 +0500

    Update src/helpers/mailer.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -22,7 +22,7 @@ export const sendEmail = async ({ email, emailType, userId }: { email: string, e
 
         const transporter = nodemailer.createTransport({
             host: process.env.NODEMAILER_HOST,
-            port: process.env.NODEMAILER_PORT,
+            port: Number(process.env.NODEMAILER_PORT),
             auth: {
                 user: process.env.NODEMAILER_USER,
                 pass: process.env.NODEMAILER_PASS
